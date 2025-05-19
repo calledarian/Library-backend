624642fb7eb8e6b4e2284e521540ee5ef7c2954a
Arian <ariankhadem4@gmail.com>
2025-05-19 15:46:02 +0700

    asd

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -3,6 +3,7 @@ import { AdminService } from './admin.service';
 import { Admin } from './admin.entity';
 import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('/admin')
 export class AdminController {
     constructor(private readonly adminService: AdminService) { }
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
